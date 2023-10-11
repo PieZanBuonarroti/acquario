5144b43a95993fc5f1511d372fddc5e4882a8557
PieZanBuonarroti <pietro.zanetello@buonarroti.tn.it>
2023-10-11 15:49:18 +0200

    feat: prova creazione Pesce Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Prova creazione pesce");
+        Pesce p = new Pesce("Squalo balena", "Onnivoro", "Gilberto");
+        System.out.println(p.toString());
+
+        
+
+    }
+
 }
