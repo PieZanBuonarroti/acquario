e7fa131a02df6b27dbce882cd573bb0968348821
PieZanBuonarroti <pietro.zanetello@buonarroti.tn.it>
2023-10-11 15:26:31 +0200

    feat: costruttore Pesce

--- src/Pesce.java
+++ src/Pesce.java
@@ -4,5 +4,28 @@ public class Pesce {
     private String dieta;
     private int eta;
     private String nome;
-    
+
+    public Pesce(String specie, String dieta, int eta, String nome){
+        if(specie.equals("")){
+            this.specie="Specie non identificata";
+        }else{
+            this.specie=specie;
+        }
+        if(dieta.equals("")){
+            this.dieta="Dieta non identificata";
+        }else{
+            this.dieta=dieta;
+        }
+        if(nome.equals("")){
+            this.nome="Nome non identificato";
+        }else{
+            this.nome=nome;
+        }
+        if(eta>0){
+            this.eta=eta;
+        }else{
+            this.eta=1;
+        }
+    }
+
 }
